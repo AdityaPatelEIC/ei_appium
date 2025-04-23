c912ee572b467c24d301e61048030b8b908eb93b
Aditya Patel <Aditya.Patel@einfochips.com>
2025-04-23 15:18:26 +0530

    correction in long press actions methods

--- base_page/element_interactions.py
+++ base_page/element_interactions.py
@@ -49,9 +49,9 @@ def long_click_element(device, locator_type, locator_value, timeout):
     try:
         element = wait_for_element(device, locator_type, locator_value, timeout)
         if element:
-            actions.click_and_hold().perform()
+            actions.click_and_hold(element).perform()
             time.sleep(0.5)
-            actions.release()
+            actions.release(element).perform()
             log.info(f"Long clicked on element with locator type {locator_type} and locator value {locator_value}")
     except Exception as e:
         log.error(f"Element not found with {locator_type} = {locator_value}. :Error-{e}")
@@ -65,7 +65,7 @@ def click_and_hold_element(device, locator_type, locator_value, hold_time, timeo
         if element:
             actions.click_and_hold(element).perform()
             time.sleep(hold_time)
-            actions.release()
+            actions.release(element).perform()
             log.info(f"Clicked on element with locator type {locator_type} and locator value {locator_value} for {hold_time} seconds")
     except Exception as e:
         log.error(f"Element not found with {locator_type} = {locator_value}. :Error-{e}")
